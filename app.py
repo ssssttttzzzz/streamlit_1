c0c7f880a583b73b1b11c99beafaeb2079f66212
ssssttttzzzz <47779363+ssssttttzzzz@users.noreply.github.com>
2022-10-23 18:14:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 import streamlit as st
 import pandas as pd
+import random as rd
 
-st.set_page_config(page_icon="📈", page_title="Crypto Dashboard")
-st.code("🃒")
+st.set_page_config(page_icon="📈", page_title="play")
+def get_mycard():
+  my_card = [None]*3
+  i = 0
+  hsk = ["♥️","♠️","♦️","♣️"]
+  paik = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+  while not my_card[2]:
+    hs = rd.randint(0,3)
+    pai = rd.randint(0,12)
+    creat_card = hsk[hs]+paik[pai]
+    if creat_card not in my_card:
+      my_card[i]=creat_card
+      i+=1
+  return my_card
+my_card = get_mycard()    
+st.title(my_card)
